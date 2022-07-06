93518eb5c95bb89c33f37c4bbb17a7bcda83d59f
a-chechetkin <a-chechetkin@yandex-team.ru>
2022-07-07 01:01:46 +0300

    ошибки фикс

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   createCard,
@@ -8,10 +9,23 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-router.post('', createCard);
+const validateCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+});
+
+const validateId = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().length(24).hex(),
+  }),
+});
+
+router.post('', validateCard, createCard);
 
 router.get('', getCards);
-router.delete('/:cardId', deleteCardById);
+router.delete('/:cardId', validateId, deleteCardById);
 
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
