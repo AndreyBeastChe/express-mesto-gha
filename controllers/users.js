628aa5af1a8b27cb0fda96f61f22e5ebfa95f7b4
a-chechetkin <a-chechetkin@yandex-team.ru>
2022-07-09 12:40:36 +0300

    правки после ревью2

--- controllers/users.js
+++ controllers/users.js
@@ -110,44 +110,47 @@ module.exports.updateAvatar = (req, res, next) => {
     });
 };
 
-// module.exports.login = (req, res, next) => {
-//   const { email, password } = req.body;
-//   User.findOne({ email }).select('+password')
-//     .then((user) => {
-//       if (!user) {
-//         next(new UnauthorizedError('Неправильный емейл или пароль'));
-//         return;
-//       }
-//       // eslint-disable-next-line consistent-return
-//       return Promise.all([
-//         user,
-//         bcrypt.compare(password, user.password),
-//       ]);
-//     })
-//     .then(([user, isPasswordCorrect]) => {
-//       if (!isPasswordCorrect) {
-//         next(new UnauthorizedError('Неправильный емейл или пароль'));
-//         return;
-//       }
-//       res.send({
-//         token: jwt.sign({ _id: user._id }, SECRET_KEY, { expiresIn: '7d' }),
-//       });
-//     });
-// };
-
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  User.findUserByCredentials(email, password)
+  User.findOne({ email }).select('+password')
     .then((user) => {
-      res.send({
-        token: jwt.sign({ _id: user._id }, SECRET_KEY, { expiresIn: '7d' }),
-      });
-    })
-    .catch((err) => {
-      if (err.statusCode === 401) {
+      if (!user) {
         next(new UnauthorizedError('Неправильный емейл или пароль'));
         return;
       }
-      next(err);
+      // eslint-disable-next-line consistent-return
+      return Promise.all([
+        user,
+        bcrypt.compare(password, user.password),
+      ]);
+    })
+    .then(([user, isPasswordCorrect]) => {
+      if (!isPasswordCorrect) {
+        next(new UnauthorizedError('Неправильный емейл или пароль'));
+        return;
+      }
+      res.send({
+        token: jwt.sign({ _id: user._id }, SECRET_KEY, { expiresIn: '7d' }),
+      });
     });
 };
+
+// module.exports.login = (req, res, next) => {
+//   const { email, password } = req.body;
+//   console.log(req.body);
+//   User.findUserByCredentials({ email }).select('+password')
+//     .then((user) => {
+//       console.log(user);
+//       res.send({
+//         token: jwt.sign({ _id: user._id }, SECRET_KEY, { expiresIn: '7d' }),
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       if (err.statusCode === 401) {
+//         next(new UnauthorizedError('Неправильный емейл или пароль'));
+//         return;
+//       }
+//       next(err);
+//     });
+// };
